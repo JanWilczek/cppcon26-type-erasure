43915321383d77d041f17de47c790e3785eb08f0
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2026-09-01 10:26:26 +0200

    feat: add progress bar

--- global-top.vue
+++ global-top.vue
@@ -8,6 +8,29 @@
     v-if="$nav.currentLayout !== 'cover' && $nav.currentLayout !== 'image'"
   >
     <Scroll/>
-  <div class="absolute bottom-5 left-0 right-5 text-right text-xs"><SlideCurrentNo/>/<SlidesTotal/></div>
+    <div class="absolute bottom-5 left-0 right-5 text-right text-xs"><SlideCurrentNo/>/<SlidesTotal/></div>
+    <div class="progress-track" aria-hidden="true">
+      <div
+        class="progress-bar"
+        :style="{ width: `${($nav.currentPage / $nav.total) * 100}%` }"
+      />
+    </div>
   </footer>
 </template>
+
+<style>
+.progress-track {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 4px;
+  z-index: 10;
+}
+
+.progress-bar {
+  height: 100%;
+  background: rgb(233, 22, 60);
+  transition: width 0.3s ease;
+}
+</style>
